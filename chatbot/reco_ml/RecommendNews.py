f67fde20eddb50f6b3a4724457193e19f279a5be
dasol <yuntrepreneur@gmail.com>
2021-08-01 15:19:37 +0900

    Update RecommendNews.py

--- chatbot/reco_ml/RecommendNews.py
+++ chatbot/reco_ml/RecommendNews.py
@@ -44,7 +44,7 @@ genre_sim_sorted_ind=genre_sim.argsort()[:,::-1]
 
 
 ######  DB 접속 #############################################
-con=pymysql.connect(host='final-chatbot.c7dwoilcj9uq.ap-northeast-2.rds.amazonaws.com',user='yejin',password='1313', db='CHAT_BOT_DB', charset='utf8')
+con=pymysql.connect(host='*************.rds.amazonaws.com',user='****',password='******', db='CHAT_BOT_DB', charset='utf8')
 cur=con.cursor()
 
 # User Table에서 User정보 싹다 뽑아오기 
